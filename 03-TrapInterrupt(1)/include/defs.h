0409fea296a5d4bef1860f108c219a21c585ca40
ds19069679773 <3402655264@qq.com>
2026-05-16 09:43:07 +0800

    Delete 03-TrapInterrupt(1) directory

--- 03-TrapInterrupt(1)/include/defs.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEFS_H
-#define DEFS_H
-
-#include "riscv.h"
-#include "sbi.h"
-#include "print.h"
-#include "types.h"
-#include "trap.h"
-#endif // DEF_H
--- 03-TrapInterrupt(1)/out/src/main.d
+++
@@ -1,4 +0,0 @@
-out/src/main.o: src/main.c include/print.h include/types.h include/trap.h \
- include/defs.h include/riscv.h include/sbi.h include/print.h \
- include/trap.h include/sbi.h \
- /usr/lib/gcc/riscv64-unknown-elf/14.2.0/include/stdint.h
